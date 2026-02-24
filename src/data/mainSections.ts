d41be6a498a93128b3f8956bd17ea639cbf8bf7f
moon <moondubu@gmail.com>
2026-02-24 14:42:15 +0900

    Fix missing mediaAspectRatio on MAIN_IP_SECTION

--- src/data/mainSections.ts
+++ src/data/mainSections.ts
@@ -44,6 +44,7 @@ export const MAIN_IP_SECTION = {
   number: "02",
   titleLine1: "No.1",
   titleLine2: "Pet IP",
+  mediaAspectRatio: "2560 / 720",
   panelLabel: "Korea No.1 Pet IP",
   panelTitleLine1: "최고의 IP, 페스룸으로",
   panelTitleLine2: "",
